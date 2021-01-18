3338511b9b77c7aa8990718970e8d166356e635a
Rashika <rashikarawat01@gmail.com>
2021-01-18 13:50:46 +0530

    added changes to master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import TopMenu from "./components/TopMenu";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LogIn from './components/LogIn';
 
 const App = () => {
 
  return(
    <React.Fragment>
      <TopMenu></TopMenu>
+     <Router>
+       <Route path ='/log-in' component ={LogIn}></Route>
+     </Router>
    </React.Fragment>
  );
 
