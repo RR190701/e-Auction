c08a9b74af4ea93b6e183275a761e9a75ace1506
Rashika <rashikarawat01@gmail.com>
2021-01-18 13:19:02 +0530

    added logIn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import TopMenu from "./components/TopMenu";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import LogIn from './components/LogIn';
 
 const App = () => {
 
  return(
    <React.Fragment>
-     <TopMenu></TopMenu>
+      <TopMenu/>
+     <Router>
+     <Route path = "/Log-in" exact component = {LogIn}/>
+     </Router>
    </React.Fragment>
  );
 
