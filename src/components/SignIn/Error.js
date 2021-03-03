2bc0ef68965ef10f5e4265667edda7c88683fa0e
Rashika <rashikarawat01@gmail.com>
2021-03-04 00:12:05 +0530

    some changes in sign up file

--- src/components/SignIn/Error.js
+++ src/components/SignIn/Error.js
@@ -7,7 +7,7 @@ const Error = ({ touched, message }) => {
   if (message) {
     return <div className="form-message invalid"><text style={{color: "red"}}>{message}</text></div>;
   }
-  return <div className="form-message valid"><text style={{color: "green"}}>all good</text></div>;
+  return <div className="form-message valid"><text style={{color: "green"}}>All good</text></div>;
 };
 
 export default Error;
--- src/components/SignIn/SignUp.js
+++ src/components/SignIn/SignUp.js
@@ -8,54 +8,61 @@ import Error from './Error'
 
 const SignUp = () => {
   const ValidationSchema = Yup.object().shape({
+
     Name: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required("Name is required"),
+    .required("*Name is required"),
+
     Zip: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Zip is required"),
+    .min(6, "Too Short!")
+    .max(7, "Invalid!")
+    .required("*Zip is required"),
   
-    UserName: Yup.string()
+    password: Yup.string()
+    .required("*Password is required.")
+    .min(8, "*Password is too short - should be 8 chars minimum.")
+    .matches(/(?=.*[0-9])/, "*Password must contain a number."),
+    
+    Age: Yup.string()
+    .required("*Age is required")
     .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Username is required"),
+    .max(50, "Too Long!"),
     
     Address: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required("Address is required"),
+    .required("*Address is required"),
+
     City: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required("City is required"),
+    .required("*City is required"),
   
   Number: Yup.string()
-  //   .required("Phone number is required")
-  //   .matches(
-  // /^([0]{1}|\+?[234]{3})([7-9]{1})([0|1]{1})([\d]{1})([\d]{7})$/g,
-  //     "Invalid phone number"
-  //   ),
-  .min(2, "Too Short!")
-  .max(50, "Too Long!")
-  .required("Number is required"),
+    .matches(
+      /\d{5}([- ]*)\d{6}/,
+      "*Invalid phone number"
+    )
+  .min(10, "Too Short!")
+  .max(13, "Too Long!")
+  .required("*Number is required"),
+
     email: Yup.string()
-    .email("Must be an email address")
-    .max(255, "Too Long!")
-    .required("Required")
+    .email("*Must be a valid email address")
+    .required("*Email is required")
 });
   
 
-  // const HandleSubmit = (e) =>{
-  //   e.preventDefault();
+  const HandleSubmit = (e) =>{
+    e.preventDefault();
 
-  //   //call the server
-  //   console.log("submitted");
-  // }
+    //call the server
+    console.log("submitted");
+  }
 
     return ( 
-      <Formik initialValues={{ Name: '', UserName: '', Address: '', City: '', email: '', Number: '', Zip: ''}}
+      <Formik initialValues={{ Name: '', password: '', Address: '', City: '', Age:'', email: '', Number: '', Zip: ''}}
       validationSchema={ValidationSchema}
       onSubmit={(values, { setSubmitting, resetForm }) => {
         setSubmitting(true);
@@ -73,18 +80,12 @@ const SignUp = () => {
         <Paper className="sign-in-form" elevation={3}>
    
           <h1 className="text-center pb-5 sign-in-heading">SIGN IN</h1>
-  <form onSubmit={handleSubmit} className="row g-3">
+  <form onSubmit={handleSubmit}  className="row g-3">
   <div className="col-md-6">
-    <label htmlFor="Name" className="form-label">Name</label>
+    <label htmlFor="Name" className="form-label">Full Name</label>
     <input type="text" name="Name" onChange={handleChange}  id="Name" onBlur={handleBlur}
 								 value={values.Name}    className="form-control"  />
                    <Error touched={touched.Name} message={errors.Name} />
-  </div>
-  <div className="col-md-6">
-    <label htmlFor="UserName" className="form-label">User Name</label>
-    <input type="text" name="UserName" id="UserName"  onChange={handleChange}  onBlur={handleBlur} value={values.UserName} 	
-							  className="form-control" />
-                 <Error touched={touched.UserName} message={errors.UserName} />
   </div>
   <div className="col-md-6">
     <label htmlFor="Number" className="form-label">Contact Number</label>
@@ -96,6 +97,12 @@ const SignUp = () => {
     <input type="text" name="email" id="email"  onChange={handleChange} value={values.email}  onBlur={handleBlur}  className="form-control"	
 							 /> <Error touched={touched.email} message={errors.email} />
   </div>
+  <div className="col-md-6">
+    <label htmlFor="password" className="form-label">Password</label>
+    <input type="text" name="password" id="password"  onChange={handleChange}  onBlur={handleBlur} value={values.password} 	
+							  className="form-control" />
+                 <Error touched={touched.password} message={errors.password} />
+  </div>
   <div className="col-md-9">
     <label htmlFor="Address" className="form-label">Full Address</label>
     <input type="text" className="form-control"  onChange={handleChange} onBlur={handleBlur} value={values.Address} id="Address" 
@@ -104,7 +111,8 @@ const SignUp = () => {
   </div>
   <div className="col-md-3">
     <label htmlFor="inputAge" className="form-label">Age</label>
-    <input type="number" className="form-control"  onChange={handleChange}  onBlur={handleBlur} id="inputAge"></input>
+    <input type="text" className="form-control"  onChange={handleChange}  onBlur={handleBlur} value={values.Age} id="inputAge"></input>
+    <Error touched={touched.Age} message={errors.Age} />
   </div>
   <div className="col-md-6">
     <label htmlFor="City" className="form-label">City</label>
@@ -115,21 +123,41 @@ const SignUp = () => {
     <label htmlFor="inputState"  className="form-label">State</label>
     <select id="inputState"  className="form-select">
       <option selected>Choose...</option>
+      <option>Andhra Pradesh</option>
+      <option>Arunachal Pradesh</option>
+      <option>Assam</option>
+      <option>Bihar</option>
+      <option>Chhattisgarh</option>
+      <option>Goa</option>
+      <option>Gujarat</option>
+      <option>Haryana</option>
+      <option>Himachal Pradesh</option>
+      <option>Jharkhand</option>
+      <option>Karnataka</option>
+      <option>Kerala</option>
+      <option>Madhya Pradesh</option>
+      <option>Maharashtra</option>
+      <option>Manipur</option>
+      <option>Meghalaya</option>
+      <option>Mizoram</option>
+      <option>Nagaland</option>
+      <option>Odisha</option>
+      <option>Punjab</option>
+      <option>Rajasthan</option>
+      <option>Sikkim</option>
+      <option>Tamil Nadu</option>
+      <option>Telangana</option>
+      <option>Tripura</option>
       <option>Uttar Pradesh</option>
+      <option>Uttarakhand</option>
+      <option>West Bengal</option>
+
     </select>
   </div>
   <div className="col-md-2">
     <label htmlfor="Zip" className="form-label">Zip</label>
     <input type="text" className="form-control" id="Zip"   onChange={handleChange}  onBlur={handleBlur}  value={values.Zip}/>
     <Error touched={touched.Zip} message={errors.Zip}/>
-  </div>
-  <div className="col-12">
-    <div className="form-check">
-      <input className="form-check-input" type="checkbox" id="gridCheck"></input>
-      <label className="form-check-label" htmlfor="gridCheck">
-        Check me out
-      </label>
-    </div>
   </div>
   <div className="col-12">
     <button type="submit" className="btn btn-primary sign-in-button"  disabled={isSubmitting}>Sign in</button>
