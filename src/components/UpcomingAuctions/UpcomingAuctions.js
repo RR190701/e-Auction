86172ad4c74bcda4fe82e22d4b8f51089c6ac513
Rashika <rashikarawat01@gmail.com>
2021-03-13 10:06:56 +0530

    Used mapped in Auction cards

--- src/components/UpcomingAuctions/UpcomingAuctions.js
+++ src/components/UpcomingAuctions/UpcomingAuctions.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React ,{useState} from "react";
 import Grid from "@material-ui/core/Grid";
 import { Card, CardHeader, CardContent, Divider } from "@material-ui/core";
 import "./style.css";
 import AuctionCard from "../../containers/AuctionCard/auctionCard";
 
 const UpcomingAuctions = () => {
+
+  const [upcomingAuctions, setUpcomingAuctions] = useState([
+    {
+      title:"Beauty Banner",
+      organisation: "Ratan Tata limited",
+      info:"  An auction for Beautiful diamond earings and rings of original titan brand.",
+      date:"18/03/2021",
+      time:"4:45 PM"
+    },
+    {
+      title:"Biker Beast",
+      organisation: "Telsa time private limited",
+      info:"An auction for super powerful and strong bikes that can run on different terrain.",
+      date:"3/04/2021",
+      time:"11:00 AM"
+    },
+    {
+      title:"Hella Commet",
+      organisation: "Surya Dev limited",
+      info:"An auction for original handmade vase and flower pots.",
+      date:"15/03/2021",
+      time:"9:00 AM"
+    },
+    {
+      title:"Bhakti Corner",
+      organisation: "Prakaram Ashram",
+      info:"  An auction for handmade gods and goddess scupltures made by students of our Ashram.",
+      date:"20/03/2021",
+      time:"1:30 PM"
+    },
+  ])
+
   return (
     <div className ="upcoming-auction-container">
-        <AuctionCard></AuctionCard>
+
+      {
+        upcomingAuctions.map( (auction) => (
+          <AuctionCard
+          title = {auction.title}
+           organisation  = {auction.organisation}
+           info = {auction.info}
+           date = {auction.date}
+           time = {auction.time}
+  
+          ></AuctionCard>
+  ))
+      }
+     
         
-        <AuctionCard></AuctionCard>
-        <AuctionCard></AuctionCard>
-        <AuctionCard></AuctionCard>
-        <AuctionCard></AuctionCard>
-        <AuctionCard></AuctionCard>
     </div>
   );
 };
--- src/containers/AuctionCard/auctionCard.js
+++ src/containers/AuctionCard/auctionCard.js
@@ -5,27 +5,26 @@ import DateRangeTwoToneIcon from "@material-ui/icons/DateRangeTwoTone";
 import ScheduleTwoToneIcon from "@material-ui/icons/ScheduleTwoTone";
 import Paper from "@material-ui/core/Paper";
 
-const AuctionCard = () => {
+const AuctionCard = ({title, organisation, info, date, time}) => {
   return (
     <Paper elevation={3} className="auction-card">
       <div className="auction-card-content">
-        <h1 className="auction-title">Beauty Banner</h1>
-        <h1 className="auction-organisation">Ratan Tata limited</h1>
+        <h1 className="auction-title">{title}</h1>
+        <h1 className="auction-organisation">{organisation}</h1>
         <p className="auction-info">
-          An auction for Beautiful diamond earings and rings of original titan
-          brand.
+        {info}
         </p>
 
         <div className="auction-card-footer">
           <div className="timings">
             <div>
               <DateRangeTwoToneIcon color="secondary"></DateRangeTwoToneIcon>
-              <span className="timing-content">18/03/2021</span>
+              <span className="timing-content">{date}</span>
             </div>
 
             <div>
               <ScheduleTwoToneIcon color="secondary"></ScheduleTwoToneIcon>
-              <span className="timing-content">4:45 PM</span>
+              <span className="timing-content">{time}</span>
             </div>
           </div>
 
