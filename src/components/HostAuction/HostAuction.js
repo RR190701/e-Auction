73d1eca76d68cd5278bf1bdd9860bfa43a3a7736
chand10-beg <aitshu.844@gmail.com>
2021-05-02 21:27:25 +0530

    host an auction

---
+++ src/components/HostAuction/HostAuction.js
@@ -0,0 +1,313 @@
+import React, { Component, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Joi from "joi-browser";
+import "./style.css";
+import axios from "axios";
+import TextField from '@material-ui/core/TextField';
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer"
+
+import { ToastContainer, toast, Zoom, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Description } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+
+  LoginButton: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: "5px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    width: "100%",
+    padding: "0 30px ",
+    // marginTop:'2rem',
+    // marginBottom:'2rem',
+  },
+}));
+const HostAuction = () => {
+    const popError = (errorMessage) => {
+
+        toast.error(errorMessage, {
+          className :"error-toast",
+          position:toast.POSITION.BOTTOM_RIGHT
+        });
+      }
+    const [auctionName, setauctionName] = useState("");
+    const [status, setstatus] = useState("");
+    const [auctioneer, setauctioneer] = useState("");
+    const [lot, setlot] = useState("");
+    const [minEstimation, setminEstimation] = useState("");
+    const [ maxEstimation, setmaxEstimation] = useState("");
+    const [location, setlocation] = useState("");
+    const [timings, settimings] = useState("");
+    const [discription, setdiscription] = useState("");
+    const [ auctionID, setauctionID] = useState("");
+    const [error, setError] = useState("");
+    const classes = useStyles();
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        try {
+          const { data } = await axios.post(
+            "/api/auction/registerAuction",
+            { auctionID, auctionName, lot, auctioneer, minEstimation, maxEstimation, discription, location, timings, status},
+            config
+          );
+          localStorage.setItem("authToken", data.token);
+        //   localStorage.setItem("username", username);
+        //   history.push("/");
+        popError("registered successfully");
+        } catch (error) {
+        //   popError(error.response.data.error);
+          setError(error.response.data.error);
+         
+          setTimeout(() => {
+            setError("");
+          }, 5000);
+        }
+      };
+    return(
+        <div>
+<Paper className="RegisterPaper" elevation={3}>
+      <>
+      
+      <ToastContainer
+      draggable ={false}
+      autoClose={3000}
+      ></ToastContainer>
+
+      </>
+      <h2 className="text-center pb-0 pt-0" style={{fontFamily: "cursive"}}>Host An Auction</h2>
+      <form onSubmit={HandleSubmit} >
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputEmail1" className="form-label">
+            Email
+          </label> */}
+          <TextField
+          required 
+
+            type="text"
+            value={auctionID}
+            onChange={(e) =>setauctionID(e.target.value)}
+           
+            label="auctionID"
+            // tabIndex={1}
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          {/* {errors.email && (
+            <div className="alert alert-danger">{errors.email}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="auction Name"
+            type="text"
+            value={auctionName}
+            onChange={(e) => setauctionName(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="auctioneer"
+            type="text"
+            value={auctioneer}
+            onChange={(e) => setauctioneer(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Lot"
+            type="text"
+            value={lot}
+            onChange={(e) => setlot(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Minimum Estimation"
+            type="text"
+            value={minEstimation}
+            onChange={(e) => setminEstimation(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Maximum Estimation"
+            type="text"
+            value={maxEstimation}
+            onChange={(e) => setmaxEstimation(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Status"
+            type="text"
+            value={status}
+            onChange={(e) => setstatus(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Timings"
+            type="text"
+            value={location}
+            onChange={(e) => setlocation(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Description"
+            type="text"
+            value={discription}
+            onChange={(e) => setdiscription(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="col-md-10">
+          {/* <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label> */}
+          <TextField
+          required
+          label="Timings"
+            type="text"
+            value={timings}
+            onChange={(e) => settimings(e.target.value)}
+            // name="password"
+            // tabIndex={3}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          {/* {errors.password && (
+            <div className="alert alert-danger">{errors.password}</div>
+          )} */}
+        </div>
+        <div className="text-center pt-3">
+          <Button type="submit" tabIndex={4} className={classes.LoginButton}>
+            Register
+          </Button>
+        </div>
+
+        {/* <div className = "helpful-link"> */}
+        {/* <Link to="/ForgetPassword" style = {{"textDecoration":"none"}} tabIndex={2}>
+              Forget Password?
+            </Link>
+           
+        <Link to="/sign-up" style = {{"marginLeft":".5rem", "textDecoration":"none"}} tabIndex={2}>
+              Create a new account.
+            </Link> */}
+          
+       
+        {/* </div> */}
+      
+      </form>
+      
+    </Paper>
+        </div>
+    )
+}
+export default HostAuction
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,13 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import {RiAuctionFill} from "react-icons/ri";
 import IconButton from "@material-ui/core/IconButton";
+import Modal from '@material-ui/core/Modal';
 import MenuIcon from "@material-ui/icons/Menu";
 import { withRouter, Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { AiTwotoneBank } from "react-icons/ai";
 import Tooltip from "@material-ui/core/Tooltip";
+import HostAuction from '../HostAuction/HostAuction';
 import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -21,7 +23,33 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     fontFamily:"Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;"
   },
+  
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+ 
 }));
+
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const Navbar = (props) => {
   const classes = useStyles();
@@ -32,6 +60,16 @@ const Navbar = (props) => {
     const { history } = props;
     history.push("/log-in");
     //console.log(props);
+  };
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -65,10 +103,17 @@ const Navbar = (props) => {
               <Button className="header" ><Link className ="top-navbar-links"  to ={`/profile/${localStorage.getItem("username")}`} >
               <CgProfile fontSize="1.8rem" padding="0" fill="white"/>
               </Link></Button>
-               <Button className="header log-out-button"   startIcon={<AiTwotoneBank/>}>
+               <Button className="header log-out-button"  onClick={handleOpen} startIcon={<AiTwotoneBank/>}>
                 Host An Auction
               </Button>
-              
+              <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <HostAuction/>
+      </Modal>
               <Button className="header log-out-button" onClick={logoutHandler}>
                 Logout
               </Button>
